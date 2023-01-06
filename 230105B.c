ed1118b632ce7ec888061f2cc1aedbc238a05696
kaxzbct <118597203+kaxzbct@users.noreply.github.com>
2023-01-06 20:11:37 +0800

    Update 230105B.c

--- 230105B.c
+++ 230105B.c
@@ -1,24 +1,18 @@
 //Finding the greatest common divisor of two positive integers.
 #include<stdio.h>
-int Min(int x, int y)
-{
-	if (x > y)
-		return y;
-	else
-		return x;
-}
 int main()
 {
-	int n = 0, m = 0, i = 0, gcd = 0;
-	printf("Please input a positive integer:");
-	scanf_s("%d", &n);
-	printf("\nPlease input another positive integer:");
-	scanf_s("%d", &m);
-	for (i = 1; i <= Min(n, m); i++)
+	int n = 0, m = 0, i = 0, tmp = 0;
+	printf("Please input two positive integer:");
+	scanf_s("%d%d", &n, &m);
+	if (n > m)
 	{
-		if ((n % i == 0) && (m % i == 0))
-			gcd = i;
+		tmp = n;
+		n = m;
+		m = tmp;
 	}
-	printf("\nThe greatest common divisor is %d.\n ", gcd);
+	if (m % n != 0)
+		n = m % n;
+	printf("\nThe greatest common divisor is %d.\n ", n);
 	return 0;
 }
